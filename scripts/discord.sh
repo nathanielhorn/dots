d0e1daec6f84e50c89f2ddcce784726fa39744f1
peter <peter>
2018-07-26 00:49:43 +0000

    initial setup

---
+++ scripts/discord.sh
@@ -0,0 +1,4 @@
+!/bin/bash sh
+
+/home/peco/appimages/discord-stable-0.0.5-x86_64.AppImage 
+
---
+++ scripts/info.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# a system info script written by yours truly
+# it's hardcoded stuff, lulz
+
+red="\e[31m"
+grn="\e[32m"
+ylw="\e[33m"
+cyn="\e[36m"
+blu="\e[34m"
+prp="\e[35m"
+bprp="\e[35;1m"
+rst="\e[0m"
+
+
+echo
+echo
+echo -e "  ${grn}    shell  ${rst}"  " bash    "
+echo -e "  ${grn}    wm     ${rst}"  " 2bwm   "
+echo -e "  ${grn}    editor ${rst}"  " vim   "
+echo -e "  ${grn}    distro ${rst}"  " crux "
+echo
+echo
